1161e848d1c0107056b7072cefba2cc983e71659
mutv625 <mutv625@gmail.com>
2026-05-27 21:52:37 +0900

    ライブラリの無視を削除（Sys.Init() が暴走するため…）

--- 01_JackCompiler/Program.cs
+++ 01_JackCompiler/Program.cs
@@ -2,8 +2,6 @@
 
 class Program
 {
-    static List<string> libraryIgnore = ["Output.jack"];
-
     static void Main(string[] args)
     {
         var envInBase = Path.Combine(AppContext.BaseDirectory, ".env");
@@ -32,15 +30,6 @@ class Program
         string libDir = Path.Combine(sourceDir, "Library");
         foreach (string file in Directory.GetFiles(libDir, "*.jack"))
         {
-            if (libraryIgnore.Contains(Path.GetFileName(file)))
-            {
-                if (File.Exists(Path.Combine(outputDir, Path.GetFileName(file))))
-                {
-                    File.Delete(Path.Combine(outputDir, Path.GetFileName(file)));
-                }
-                continue;
-            }
-
             string destFile = Path.Combine(outputDir, Path.GetFileName(file));
             File.Copy(file, destFile, true);
         }
