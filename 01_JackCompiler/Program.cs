293ba57a811b96a8a4662f07d00f13779a9bda59
mutv625 <mutv625@gmail.com>
2026-05-27 20:47:45 +0900

    コンパイラのパスの指定の更新

--- 01_JackCompiler/Program.cs
+++ 01_JackCompiler/Program.cs
@@ -1,8 +1,18 @@
-﻿class Program
+﻿using DotNetEnv;
+
+class Program
 {
     static void Main(string[] args)
     {
-        string sourceDir = args.Length > 0 ? args[0] : @"C:\Users\mutv6\Desktop\Creative Programming P\01_JackCompiler\Source";
+        var envInBase = Path.Combine(AppContext.BaseDirectory, ".env");
+        if (File.Exists(envInBase)) Env.Load(envInBase);        
+
+        string? sourceDir = args.Length > 0 ? 
+            args[0] :
+            Env.GetString("SOURCE_DIR");
+
+        if (sourceDir == null) throw new ArgumentException("Source directory is not provided.");
+
 
         string outputDir = Path.Combine(sourceDir, "Output");
         if (Directory.Exists(outputDir))
@@ -15,15 +25,54 @@
             Directory.CreateDirectory(outputDir);
         }
 
-        foreach (string file in Directory.GetFiles(sourceDir))
+        // # Library/*.jack -> Output/*.jack
+        // ! Library/*.jack are not allowed to be modified.
+        string libDir = Path.Combine(sourceDir, "Library");
+        foreach (string file in Directory.GetFiles(libDir, "*.jack"))
         {
             string destFile = Path.Combine(outputDir, Path.GetFileName(file));
             File.Copy(file, destFile, true);
         }
 
-        // 
+        // # Source/Input/*.jack -> Output/*.jack
+        string inputDir = Path.Combine(sourceDir, "Input");
+        foreach (string file in Directory.GetFiles(inputDir, "*.jack"))
+        {
+            string destFile = Path.Combine(outputDir, Path.GetFileName(file));
+            File.Copy(file, destFile, false);
+        }
+
+        // # Output/*.jack -> Output/*.xml,.vm
         foreach (string file in Directory.GetFiles(outputDir, "*.jack"))
         {
+            Console.WriteLine($"Processing {file}...");
+
+            // DEBUG Tokenizer
+            Tokenizer tokenizer = new Tokenizer(file);
+            TokenXmlWriter tokenXmlWriter = new TokenXmlWriter(Path.Combine(outputDir, $"{Path.GetFileNameWithoutExtension(file)}_tokens.xml"));
+                while (tokenizer.HasMoreTokens)
+                {
+                    tokenizer.Advance();
+                    switch (tokenizer.CTokenType)
+                    {
+                        case TokenType.KEYWORD:
+                            tokenXmlWriter.WriteLine(tokenizer.CTokenType, tokenizer.CKeyword.ToString());
+                            break;
+                        case TokenType.SYMBOL:
+                            tokenXmlWriter.WriteLine(tokenizer.CTokenType, tokenizer.CSymbol.ToString());
+                            break;
+                        case TokenType.IDENTIFIER:
+                            tokenXmlWriter.WriteLine(tokenizer.CTokenType, tokenizer.CIdentifier);
+                            break;
+                        case TokenType.INTEGER_CONSTANT:
+                            tokenXmlWriter.WriteLine(tokenizer.CTokenType, tokenizer.CIntVal.ToString());
+                            break;
+                        case TokenType.STRING_CONSTANT:
+                            tokenXmlWriter.WriteLine(tokenizer.CTokenType, tokenizer.CStringVal);
+                            break;
+                    }
+                }
+
             Parser parser = new Parser(file);
             Class ast = parser.Compile();
 
@@ -34,11 +83,27 @@
             compiler.Compile();
         }
 
+        // # Output/*.vm (files) -> Output/_Out.asm (single .asm file)
         VmTranslator vmTranslator = new VmTranslator(outputDir);
         vmTranslator.TranslateAll();
 
+        // DEBUG VM Linker
+        using (StreamWriter sw = new StreamWriter(Path.Combine(outputDir, "_Out.vm")))
+        {
+            foreach (string file in Directory.GetFiles(outputDir, "*.vm"))
+            {
+                if (Path.GetFileName(file) == "_Out.vm") continue;
+                
+                using (StreamReader sr = new StreamReader(file))
+                {
+                    sw.Write(sr.ReadToEnd());
+                }
+            }
+        }
+            
+
+        // # Output/_Out.asm -> Output/_Out.hack
         Assembler assembler = new Assembler(outputDir);
         assembler.AssembleAll();
-            
     }
 }
